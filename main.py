0fbfa400e17ef8e4a9dbde2b4c3d8033bcc5835a
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2024-12-06 08:28:38 -0500

    Actividad de modularización

--- main.py
+++ main.py
@@ -1,30 +1,8 @@
-#VARIABLES
-#TIPOS DE DATOS
-# - String = cadenas de caracteres "str"
-# - Char = caracter "str"
-# - Number = enteros "int", decimal "float"
-# - Bolean = datos lógicos "bool"
-name = "Juan David"
-age = 16
-height = 1.70
-trainer = False
+from design.menu import design
 
-#Conjunto de datos
-#Array = conjunto de datos de una dimensión "list"
-hobbies = list()
-hobbies = ["Dormir", "Comer", "Programar", "Música"]
-#print(hobbies)
-
-#MODIFICAR UN DATO DE UNA LISTA
-#hobbies[2] = "Ducharme"
-
-#SOLICITAR DAYTOS EN LA CONSOLA Y GUARDARLOS EN LA LISTA
-#dato = input["Ingrese otro pasatiempo: "]
-#Hobbies.append(dato)
-#print(hobbies)
-
-#Eliminar el último dato de la lista
-#hobbies.pop(-1)
-
-#Eliminar un dato que desee
-#hobbies.pop(2)
+while True:
+    opc = design()
+    if opc == 0:
+        break
+    else:
+        break
