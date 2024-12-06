e32f1177e846dbc0f9eb4b4cfa379d9e34683827
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2024-12-06 07:31:07 -0500

    Temas vistos; variables, tipos de datos, modificar, añadir y eliminar un dato de una lista

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello World")
+#VARIABLES
+#TIPOS DE DATOS
+# - String = cadenas de caracteres "str"
+# - Char = caracter "str"
+# - Number = enteros "int", decimal "float"
+# - Bolean = datos lógicos "bool"
+name = "Juan David"
+age = 16
+height = 1.70
+trainer = False
+
+#Conjunto de datos
+#Array = conjunto de datos de una dimensión "list"
+hobbies = list()
+hobbies = ["Dormir", "Comer", "Programar", "Música"]
+#print(hobbies)
+
+#MODIFICAR UN DATO DE UNA LISTA
+#hobbies[2] = "Ducharme"
+
+#SOLICITAR DAYTOS EN LA CONSOLA Y GUARDARLOS EN LA LISTA
+#dato = input["Ingrese otro pasatiempo: "]
+#Hobbies.append(dato)
+#print(hobbies)
+
+#Eliminar el último dato de la lista
+#hobbies.pop(-1)
+
+#Eliminar un dato que desee
+#hobbies.pop(2)
